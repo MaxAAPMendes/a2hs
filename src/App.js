25a5a99e05c41c0126c253b32f33a3f7ea75f9c5
MaxAAPMendes <max.sys.pinheiro@gmail.com>
2022-11-21 18:43:25 -0300

    inicial

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+function App() {
+  let deferredPrompt;
+  const addBtn = document.querySelector(".add-button");
+  addBtn.style.display = "none";
+
+  window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = "block";
+  
+    addBtn.addEventListener("click", (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = "none";
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        deferredPrompt = null;
+      });
+    });
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Add app a área de trabalho</h1>
+        tetste
+        <button class="add-button">Add to home screen</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
