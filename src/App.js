c024c03c0d4a27b16921c13e6fc30ba44e31b91d
MaxAAPMendes <max.sys.pinheiro@gmail.com>
2022-11-22 18:04:12 -0300

    modificado start_url

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ function App() {
     addBtn.addEventListener("click", (e) => {
       // hide our user interface that shows our A2HS button
       addBtn.style.display = "none";
+      addBtn.style.background = "gray";
       // Show the prompt
       deferredPrompt.prompt();
       // Wait for the user to respond to the prompt
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === "accepted") {
           console.log("User accepted the A2HS prompt");
+          alert("aceitou");
         } else {
           console.log("User dismissed the A2HS prompt");
+          alert("dimiss");
         }
         deferredPrompt = null;
       });
